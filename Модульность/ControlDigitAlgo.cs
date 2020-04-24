7a5061273a91f520c4cfd9de950bafd3627bb074
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-04-25 02:14:59 +0300

    Add files via upload

---
+++ Модульность/ControlDigitAlgo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SRP.ControlDigit
+{
+	public static class ControlDigitAlgo
+	{
+		public static int Upc(long number)
+		{
+			int sum = 0;
+			int factor = 3;
+			do
+			{
+				int digit = (int)(number % 10);
+				sum += factor * digit;
+				factor = 4 - factor;
+				number /= 10;
+
+			}
+			while (number > 0);
+
+			int result = sum % 10;
+			if (result != 0)
+				result = 10 - result;
+			return result;
+		}
+
+		public static int Isbn10(long number)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static int Isbn13(long number)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
