05c24488e229d6af202112d805846a4c26f6a5a9
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-05-16 11:31:26 +0300

    Add files via upload

---
+++ Управление ресурсами/APIObject.cs
@@ -0,0 +1,32 @@
+﻿// Вставьте сюда финальное содержимое файла APIObject.cs
+using System;
+
+namespace Memory.API
+{
+    public class APIObject : IDisposable
+    {
+        #region IDisposable Support
+        private readonly int pupaAndLupa;
+        private bool disposedValue = false; // Для определения избыточных вызовов
+
+        public APIObject(int number)
+        {
+            this.pupaAndLupa = number;
+            MagicAPI.Allocate(number);
+        }
+
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                MagicAPI.Free(pupaAndLupa);
+                disposedValue = true;
+            }
+        }
+
+        ~APIObject() { if (!disposedValue) MagicAPI.Free(pupaAndLupa); }
+        #endregion
+    }
+}
+
+
