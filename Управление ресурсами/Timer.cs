faffb0c0964ca24bd2e4fbc7352f177c36a97a80
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-05-28 10:57:15 +0300

    Add files via upload

---
+++ Управление ресурсами/Timer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory.Timers
+{
+    public class Timer : IDisposable
+    {
+        private static int responseTimer = 0;
+        public static string Report { get => reportBuilder.ToString(); set { } }
+        private Stopwatch tick = new Stopwatch();
+
+        private string nameTime;
+        private bool disposedValue;
+
+        private static StringBuilder reportBuilder = new StringBuilder();
+        private static int parents = -1;
+
+
+        public Timer(string name = "")
+        {
+            _ = name == "" ? nameTime = "*" : nameTime = name;
+            if (parents == -1) reportBuilder = new StringBuilder();
+            tick.Start();
+            parents++;
+        }
+        public static Timer Start(string name = "") => new Timer(name);
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                tick.Stop();
+                responseTimer += (int)tick.ElapsedMilliseconds;
+                var spacesBeforeName = new string(' ', parents * 4);
+                var spacesAfterName = new string(' ', 20 - nameTime.Length - parents * 4);
+
+                var result = spacesBeforeName + nameTime + spacesAfterName + ": "
+                    + tick.ElapsedMilliseconds.ToString() + "\n";
+
+                if (CheckRest(result)) return;
+                reportBuilder.Append(result);
+                parents--;
+                disposedValue = true;
+            }
+        }
+        private bool CheckRest(string result)
+        {
+            if (reportBuilder.Length != 0 && reportBuilder[parents * 4] == ' ')
+            {
+                var res = responseTimer - tick.ElapsedMilliseconds;
+                reportBuilder.Insert(0, result);
+                reportBuilder.Append(new string(' ', parents * 4 + 4) + "Rest"
+                    + new string(' ', 20 - 8 - parents * 4) + ": " +
+                        res.ToString() + "\n");
+                parents--;
+                return true;
+            }
+            return false;
+        }
+        ~Timer()
+        {
+
+        }
+    }
+}
