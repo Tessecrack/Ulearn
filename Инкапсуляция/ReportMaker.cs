bf1d5b864147005f637c0b9ca9af64bcf1b4c1fd
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-07-07 09:06:00 +0300

    Update ReportMaker.cs

--- Инкапсуляция/ReportMaker.cs
+++ Инкапсуляция/ReportMaker.cs
@@ -1,75 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace Incapsulation.Failures
 {
     public enum FailureType
     {
         UnexpectedShutdown,
         ShortNonResponding,
-        HardwareFailures,
-        ConnectionProblems
+        HardwateFailure,
+        ConnectionProblem
     }
 
-    public struct Device
+    public class Device
     {
-        public string DeviceId;
+        public int DeviceId;
         public string DeviceName;
-        public FailureType Failure;
-        public Device(string id, string name, FailureType failureType = FailureType.ShortNonResponding)
+
+        public Device(int id, string name)
         {
-            DeviceName = name;
             DeviceId = id;
-            Failure = failureType;
+            DeviceName = name;
         }
     }
 
     public class ReportMaker
     {
-        public static bool GetEarlier(DateTime now, DateTime myTimes) => now > myTimes;
-        public static bool IsFailureSerious(int failureType) => failureType % 2 == 0;
-        public static DateTime[] GetMyTimes(object[][] v)
-        {
-            int length = v.GetLength(0);
-            DateTime[] myTimes = new DateTime[length];
-            for (int i = 0; i < length; i++)
-                myTimes[i] = new DateTime(year: (int)v[i][2], month: (int)v[i][1], day: (int)v[i][0]);
-            return myTimes;
-        }
-
-        public static HashSet<Device> GetMyDevices(List<Dictionary<string, object>> devices)
-        {
-            Device oneDevice = new Device("", "");
-            HashSet<Device> myDevices = new HashSet<Device>();
-            foreach (var device in devices)
-            {
-                foreach (var element in device)
-                {
-                    if (element.Key == "DeviceId") oneDevice.DeviceId = element.Value.ToString();
-                    if (element.Key == "Name") oneDevice.DeviceName = element.Value.ToString();
-                }
-                myDevices.Add(oneDevice);
-            }
-            return myDevices;
-        }
-
-        public static HashSet<Device> GetProblematicDevice(HashSet<Device> devices,
-            int[] failures,
-            int[] deviceId,
-            DateTime dateTime,
-            DateTime[] myTimes)
-        {
-            HashSet<Device> problematicDevices = new HashSet<Device>();
-            for (int i = 0; i < failures.Length; i++)
-                if (IsFailureSerious(failures[i]) && GetEarlier(dateTime, myTimes[i]))
-                    foreach (var device in devices)
-                        if (deviceId[i] == int.Parse(device.DeviceId))
-                            problematicDevices.Add(new Device(device.DeviceId, 
-                                device.DeviceName, 
-                                (FailureType)failures[i]));
-            return problematicDevices;
-        }
-
         public static List<string> FindDevicesFailedBeforeDateObsolete(
             int day,
             int month,
@@ -79,19 +34,39 @@ namespace Incapsulation.Failures
             object[][] times,
             List<Dictionary<string, object>> devices)
         {
-            HashSet<Device> myDevices = GetMyDevices(devices);
-            DateTime dateTime = new DateTime(day: day, month: month, year: year);
-            DateTime[] myTimes = GetMyTimes(times);
-            HashSet<Device> problematicDevices = GetProblematicDevice(myDevices, failureTypes, deviceId, dateTime, myTimes);
-            return FindDevicesFailedBeforeDate(problematicDevices, dateTime);
+            var dateTimeSpec = new DateTime(year, month, day);
+            var dateTimesFail = times.Select(time => new DateTime((int)time[2], (int)time[1], (int)time[0])).ToArray();
+            var failEnum = failureTypes.Select(fail => (FailureType) fail).ToArray();
+            var listOfDevices = devices
+                .Select(device => new Device((int)device["DeviceId"], device["Name"] as string))
+                .ToList();
+            var problematicDevices = new HashSet<int>();
+            for (int i = 0; i < failureTypes.Length; i++)
+            {
+                if (IsFailureSerious(failEnum[i]) && IsEarlier(dateTimeSpec, dateTimesFail[i]))
+                {
+                    problematicDevices.Add(deviceId[i]);
+                }
+            }
+            return FindDevicesFailedBeforeDate(problematicDevices, listOfDevices, dateTimeSpec);
         }
 
-        public static List<string> FindDevicesFailedBeforeDate(HashSet<Device> problematicDevices, DateTime dateTime)
+        public static List<string> FindDevicesFailedBeforeDate(HashSet<int> problematicDevices,
+            List<Device> devices, DateTime dateTime)
         {
-            List<string> result = new List<string>();
-            foreach (var device in problematicDevices)
-                result.Add(device.DeviceName);
+            var result = new List<string>();
+            foreach (var device in devices)
+            {
+                if (problematicDevices.Contains(device.DeviceId))
+                {
+                    result.Add(device.DeviceName);
+                }
+            }
             return result;
         }
+
+        public static bool IsFailureSerious(FailureType failureType) => (int)failureType % 2 == 0;
+
+        public static bool IsEarlier(DateTime specifiedTime, DateTime failTime) => failTime < specifiedTime;
     }
 }
