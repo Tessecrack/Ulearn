e4e81d6cc7352ddc91f31c8afcd504e22ea3226e
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-07-11 11:11:50 +0300

    Update BinaryTree.cs

--- Generics/BinaryTree.cs
+++ Generics/BinaryTree.cs
@@ -1,98 +1,83 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace Generics.BinaryTrees
 {
-    public class BinaryTree<TValue> : IEnumerable<TValue>
-        where TValue : IComparable
+    public static class BinaryTree
     {
-        public BinaryTree() { }
-        public class Node
+        public static BinaryTree<T> Create<T>(params T[] values) where T : IComparable
         {
-            public Node LeftSide { get; set; }
-            public Node RightSide { get; set; }
-            public TValue Value { get; set; }
-            public Node(TValue value)
-            {
-                Value = value;
-                LeftSide = null;
-                RightSide = null;
-            }
-        }
-
-        public Node Root { get; set; }
-        public Node Left => Root.LeftSide;
-        public Node Right => Root.RightSide;
-        public TValue Value { get => Root.Value; set { } }
-        public int Count { get; set; }
-
-        private bool IsNull(Node node) => node == null;
-        public void Add(TValue value)
-        {
-            if (Root == null) Root = new Node(value);
-            else
-            {
-                Node currentNode = Root;
-                while (true)
-                {
-                    int comparator = currentNode.Value.CompareTo(value);
-                    if (comparator >= 0)
-                    {
-                        if (IsNull(currentNode.LeftSide))
-                        { currentNode.LeftSide = new Node(value); return; }
-                        else currentNode = currentNode.LeftSide;
-                    }
-                    else 
-                    {
-                        if (IsNull(currentNode.RightSide))
-                        { currentNode.RightSide = new Node(value); return; }
-                        else currentNode = currentNode.RightSide;
-                    }
-                }
-            }
-        }
-
-        public IEnumerator<TValue> GetEnumerator()
-        {
-            if (Root != null)
-            {
-                var currentNode = Root;
-                var nodes = new Stack<Node>();
-                nodes.Push(currentNode);
-                bool flag = true;
-                while (nodes.Count > 0)
-                {
-                    if (flag)
-                    {
-                        while (currentNode.LeftSide != null)
-                        {
-                            nodes.Push(currentNode);
-                            currentNode = currentNode.LeftSide;
-                        }
-                    }
-                    yield return currentNode.Value;
-                    flag = currentNode.RightSide != null;
-                    currentNode = flag ? currentNode.RightSide : nodes.Pop();
-                }
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
+            var tree = new BinaryTree<T>();
+            foreach (var value in values)
+                tree.Add(value);
+            return tree;
         }
     }
 
-    public static class BinaryTree
+    public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable
     {
-        public static BinaryTree<TValue> Create<TValue>(params TValue[] elements)
-            where TValue : IComparable
+        private SortedSet<T> sortedSet = new SortedSet<T>();
+        public class Node
         {
-            var wood = new BinaryTree<TValue>();
-            foreach (var element in elements)
-                wood.Add(element);
-            return wood;
+            internal T Value;
+            internal Node Left;
+            internal Node Right;
+            public Node(T value)
+            {
+                this.Value = value;
+                Left = null;
+                Right = null;
+            }
         }
+
+        private Node Root { get; set; }
+        public T Value => Root.Value;
+        public Node Left => Root.Left;
+        public Node Right => Root.Right;
+        public void Add(T element)
+        {
+            if (Root == null)
+            {
+                Root = new Node(element);
+                sortedSet.Add(element);
+                return;
+            }
+
+            Node currentNode = Root;
+            while (true)
+            {
+                if ((element.CompareTo(currentNode.Value) == -1 || element.CompareTo(currentNode.Value) == 0) 
+                    && CheckNode(element, ref currentNode, ref currentNode.Left)) 
+                    return;
+                if (element.CompareTo(currentNode.Value) == 1
+                    && CheckNode(element, ref currentNode, ref currentNode.Right))
+                    return;
+            }
+        }
+
+        private bool CheckNode(T element, ref Node currentNode, ref Node nextNode)
+        {
+            if (nextNode != null)
+            {
+                currentNode = nextNode;
+                return false;
+            }
+            else
+            {
+                nextNode = new Node(element);
+                sortedSet.Add(element);
+                return true;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(var element in sortedSet)
+                yield return element;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
