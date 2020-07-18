90c10ab47b76699cfee40a465f8123eb4444482a
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-07-18 12:10:55 +0300

    Update ValueType.cs

--- DDD/ValueType.cs
+++ DDD/ValueType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Ddd.Taxi.Domain;
+using System.Linq;
 using System.Reflection;
 
 namespace Ddd.Infrastructure
@@ -9,63 +10,51 @@ namespace Ddd.Infrastructure
     public class ValueType<T> where T : class
     {
         private readonly PropertyInfo[] properties;
-        public ValueType() { properties = this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance); }
+
+        public ValueType() => properties = typeof(T)
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
         public override bool Equals(object obj)
         {
-            if (!(obj is ValueType<T>)) return false;
-            for (int i = 0; i < properties.Length; i++)
+            if (!(obj is T) 
+                || !(obj is ValueType<T> downCast) 
+                || downCast.properties.Length != properties.Length) return false;
+
+            for (var i = 0; i < properties.Length; i++)
             {
-                var thisSample = properties[i].GetValue(this);
-                var thatSample = properties[i].GetValue(obj);
-                if (thisSample == null && thatSample == thisSample) continue;
-                if (!thisSample.Equals(thatSample)) return false;
+                var valueThisProp = properties[i].GetValue(this) ;
+                var valueObjProp = downCast.properties[i].GetValue(downCast);
+                if (valueThisProp == null && valueObjProp == null) continue;
+                if (!valueThisProp.Equals(valueObjProp)) return false;
             }
             return true;
         }
 
-        public bool Equals(Ddd.Taxi.Domain.PersonName thatPerson)
-        {
-            if (thatPerson == null || thatPerson.GetType() != this.GetType()) return false;
-            var thisPerson = this as Ddd.Taxi.Domain.PersonName;
-            if (thisPerson == null && thatPerson == thisPerson) return false;
-            if (thatPerson.FirstName == thisPerson.FirstName
-                && thatPerson.LastName == thisPerson.LastName) return true;
-            return false;
-        }
-
+        public bool Equals(PersonName personName) => Equals((object)personName);
         public override int GetHashCode()
         {
-            int hashCode = 0;
+            int code = 0;
             for (int i = 0; i < properties.Length; i++)
             {
-                var element = properties[i].GetValue(this);
-                var coef = i+1;
                 unchecked
                 {
-                    if (i > 0) coef = 16777619;
-                    if (!(element == null)) hashCode += element.GetHashCode() * coef;
+                    if (!(properties[i].GetValue(this) == null))
+                    {
+                        var coef = i > 0 ? i + 1 : 16777;
+                        code += properties[i].GetValue(this).GetHashCode() * coef;
+                    }
                 }
             }
-            return hashCode;
+            return code;
         }
 
         public override string ToString()
         {
-            bool flag = this is Ddd.Taxi.Domain.Address;
-            var result = GetType().Name + "(";
-            if (flag)
-            {
-                for (int i = properties.Length - 1; i >= 1; i--)
-                    result += properties[i].Name + ": " + properties[i].GetValue(this) + "; ";
-                result += properties[0].Name + ": " + properties[0].GetValue(this) + ")";
-            }
-            else
-            {
-                for (int i = 0; i < properties.Length - 1; i++)
-                    result += properties[i].Name + ": " + properties[i].GetValue(this) + "; ";
-                result += properties[properties.Length - 1].Name + ": " + properties[properties.Length - 1].GetValue(this) + ")";
-            }
-            return result;
+            var typeName = typeof(T).FullName.Split('.', '+').Last();
+            var listOfProperties = properties
+                .Select(element => element.Name + ": " + element.GetValue(this))
+                .OrderBy(element => element[0]).Aggregate((s1, s2) => s1 + "; " + s2);
+            return typeName + "(" + listOfProperties + ")";
         }
     }
 }
