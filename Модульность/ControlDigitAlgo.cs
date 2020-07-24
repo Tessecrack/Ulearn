c86fbf5e8156b8d465e9df7e5accdf9eeeaeb0d9
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-07-24 12:53:23 +0300

    Update ControlDigitAlgo.cs

--- Модульность/ControlDigitAlgo.cs
+++ Модульность/ControlDigitAlgo.cs
@@ -1,55 +1,44 @@
-﻿// Вставьте сюда финальное содержимое файла ControlDigitAlgo.cs
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 
 namespace SRP.ControlDigit
 {
-    public static class ControlDigitAlgo
+	public static class ControlDigitAlgo
+	{
+		public static int Upc(long number)
+		{
+			return number.GetDigitalSum(3).GetControlNumber(10);
+		}
+
+		public static int Isbn10(long number)
+		{
+			var controlDigit = number.GetDigitalSum(2, false).GetControlNumber(11);
+			return controlDigit == 10 ? 'X' : char.Parse(controlDigit.ToString());
+		}
+
+		public static int Isbn13(long number)
+		{
+			return number.GetDigitalSum(1).GetControlNumber(10);
+		}
+	}
+
+	public static class Extensions
     {
-        public static int Upc(long number)
-            => number.GetDigitsOfNumber().GetWeightnessSum("UPC", 3).GetControlDigit(10); // 3
-
-        public static char Isbn10(long number)
+		public static int GetDigitalSum(this long number, int factor, bool check = true)
         {
-            var res = number.GetDigitsOfNumber().GetWeightnessSum("ISBN10", 2).GetControlDigit(11);
-            return res == 10 ? 'X' : char.Parse(res.ToString());
+			var sum = 0;
+			do
+			{
+				var value = (int)(number % 10);
+				sum += factor * value;
+				factor = check ? 4 - factor : ++factor;
+				number /= 10;
+			}
+			while (number > 0);
+			return sum;
         }
 
-        public static int Isbn13(long number) // 1
-            => number.GetDigitsOfNumber().GetWeightnessSum("ISBN13", 1).GetControlDigit(10);
-    }
-
-    public static class Extensions
-    {
-        public static List<int> GetDigitsOfNumber(this long number)
-        {
-            var list = new List<int>();
-            do
-            {
-                var digit = (int)(number % 10);
-                list.Add(digit);
-                number /= 10;
-            } while (number > 0);
-            return list;
-        }
-
-        public static int GetWeightnessSum(this List<int> digitOfNumbers,
-                                           string nameCheckDigit, int coef)
-        {
-            var sum = 0;
-            foreach (var digit in digitOfNumbers)
-            {
-                sum += coef * digit;
-                if (nameCheckDigit == "UPC" || nameCheckDigit == "ISBN13") coef = 4 - coef;
-                else coef++;
-            }
-            return sum;
-        }
-
-        public static int GetControlDigit(this int number, int coef) 
-			=> number % coef != 0 ? coef -  number % coef :  number % coef;
-    }
+		public static int GetControlNumber(this int number, int coef)
+			=> number % coef == 0 ? number % coef : coef - number % coef;
+	}
 }
-
-
