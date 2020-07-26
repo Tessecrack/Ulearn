fa990a0875d7ca9e98830734f2e49150c28ef1d1
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-07-26 20:42:34 +0300

    Update CustomCompressionStream.cs

--- Работа с файлами/CustomCompressionStream.cs
+++ Работа с файлами/CustomCompressionStream.cs
@@ -24,25 +24,20 @@ namespace Streams.Compression
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            int subCounter;
-            for (subCounter = 0; subCounter < repeatByteStock; subCounter++)
-                buffer[subCounter] = (byte)valueByteStock;
-            valueByteStock = -1;
-            repeatByteStock = -1;
+            int subCounter = HelpToAddBytes(buffer);
             for (counter = subCounter; counter < count + offset;)
             {
                 var valueByte = baseStream.ReadByte();
                 var repeatByte = baseStream.ReadByte();
-                if (repeatByte == -1)
-                    return valueByte == -1 ? counter : throw new InvalidOperationException();
+                if (repeatByte == -1) return valueByte == -1 ?
+                        counter : throw new InvalidOperationException();
                 for (int i = 0; i < repeatByte; i++)
                 {
                     if (counter + offset == buffer.Length)
                     {
                         valueByteStock = valueByte;
                         repeatByteStock = repeatByte - i;
-                        if (offset > 0)
-                            buffer[buffer.Length - 1] = 0;
+                        if (offset > 0) buffer[buffer.Length - 1] = 0;
                         return counter - offset;
                     }
                     buffer[counter++ + offset] = (byte)valueByte;
@@ -53,7 +48,7 @@ namespace Streams.Compression
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            for (int i = offset; i < count; i++) 
+            for (int i = offset; i < count; i++)
             {
                 byte countRepeatByte = 1;
                 while (i < buffer.Length - 1 && buffer[i] == buffer[i + 1] && countRepeatByte < byte.MaxValue)
@@ -62,9 +57,19 @@ namespace Streams.Compression
                     i++;
                 }
                 baseStream.WriteByte(buffer[i]);
-                baseStream.WriteByte(i == buffer.Length - 1 ? 
+                baseStream.WriteByte(i == buffer.Length - 1 ?
                     (byte)(countRepeatByte - offset) : countRepeatByte);
             }
+        }
+
+        private int HelpToAddBytes(byte[] buffer)
+        {
+            int subCounter;
+            for (subCounter = 0; subCounter < repeatByteStock; subCounter++)
+                buffer[subCounter] = (byte)valueByteStock;
+            valueByteStock = -1;
+            repeatByteStock = -1;
+            return subCounter;
         }
 
         public override bool CanRead => read;
